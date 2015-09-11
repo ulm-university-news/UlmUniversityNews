38206834f051355701eba01eecb06f2ad966d0f6
Philipp Speidel <ulm.university.news@gmail.com>
2015-09-11 20:02:38 +0200

    Generierung der TokenAlreadyExistsException

---
+++ src/main/java/ulm/university/news/util/TokenAlreadyExistsException.java
@@ -0,0 +1,32 @@
+package ulm.university.news.util;
+
+/**
+ * The TokenAlreadyExistsException is thrown when the uniqueness of the access token is harmed. An access token needs
+ * to be unique to unambiguously identify an user or moderator in the whole system.
+ *
+ * @author Matthias Mak
+ * @author Philipp Speidel
+ */
+public class TokenAlreadyExistsException extends Exception {
+
+    /**
+     * Generates a TokenAlreadyExistsException with a given error message.
+     *
+     * @param message The description of the error which caused this Exception.
+     */
+    public TokenAlreadyExistsException(String message){
+        super(message);
+    }
+
+    /**
+     * Generates a TokenAlreadyExistsException with a given error message and the Throwable which caused this
+     * Exception.
+     *
+     * @param message The description of the error which caused this Exception.
+     * @param cause An instance of a Throwable object which caused this Exception.
+     */
+    public TokenAlreadyExistsException(String message, Throwable cause){
+        super(message,cause);
+    }
+
+}
