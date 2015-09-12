4025f8c63c98465f664fe9548a906bc63f27330f
Philipp Speidel <ulm.university.news@gmail.com>
2015-09-12 13:44:25 +0200

    ModeratorDatabaseManager hinzugefügt

---
+++ src/main/java/ulm/university/news/manager/database/ModeratorDatabaseManager.java
@@ -0,0 +1,24 @@
+package ulm.university.news.manager.database;
+
+/**
+ * TODO
+ *
+ * @author Matthias Mak
+ * @author Philipp Speidel
+ */
+public class ModeratorDatabaseManager {
+
+    /**
+     * Creates an instance of the ModeratorDatabaseManager class.
+     */
+    public ModeratorDatabaseManager(){
+
+    }
+
+
+    public boolean isValidModeratorToken(String accessToken){
+        // TODO
+        return false;
+    }
+
+}
