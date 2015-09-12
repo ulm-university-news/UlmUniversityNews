31e2fb7d6f80bc0aa69214676fb393e8202e6663
Philipp Speidel <ulm.university.news@gmail.com>
2015-09-12 13:22:59 +0200

    added TokenType enum

---
+++ src/main/java/ulm/university/news/util/TokenType.java
@@ -0,0 +1,15 @@
+package ulm.university.news.util;
+
+/**
+ * The TokenType defines to which resources a received token belongs. If it is of type USER, it identifies an user
+ * account. If it is of type MODERATOR, it identifies a moderator account. If the received token doesn't match one
+ * of these cases, it is considered invalid and the request needs to be rejected.
+ *
+ * @author Matthias Mak
+ * @author Philipp Speidel
+ */
+public enum TokenType {
+    USER, MODERATOR, INVALID;
+
+    public static final TokenType values[] = values();
+}
