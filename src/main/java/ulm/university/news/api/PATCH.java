34c464c74d0b408884bea4aeb001e9d5ac0efcbd
Philipp Speidel <ulm.university.news@gmail.com>
2015-09-13 12:10:38 +0200

    Hinzufügen von @PATCH Annotation

---
+++ src/main/java/ulm/university/news/api/PATCH.java
@@ -0,0 +1,20 @@
+package ulm.university.news.api;
+
+import javax.ws.rs.HttpMethod;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Creating an annotation @PATCH which can be used within the Jersey framework to map incoming HTTP PATCH requests
+ * to the handler methods.
+ *
+ * @author Matthias Mak
+ * @author Philipp Speidel
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod("PATCH")
+public @interface PATCH {
+}
