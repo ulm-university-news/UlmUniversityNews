38edb33d918f02c87d36b28cc2d0ad0e474ba4eb
Philipp Speidel <ulm.university.news@gmail.com>
2015-10-12 13:29:09 +0200

    WindowsPhone push notifications implementiert (not yet tested)

--- src/main/java/ulm/university/news/manager/push/PushManager.java
+++ src/main/java/ulm/university/news/manager/push/PushManager.java
@@ -1,18 +1,20 @@
 package ulm.university.news.manager.push;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ulm.university.news.data.PushMessage;
 import ulm.university.news.data.User;
 import ulm.university.news.data.enums.PushType;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -20,6 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * The PushManager class is used to send push messages to app clients. So far, Android and Windows clients are
@@ -36,8 +39,21 @@ public class PushManager {
     /** The access key for the GCM server retrieved from properties file. */
     private String GCM_API_KEY = null;
 
+    /** The secure package identifier which identifies a specific Windows Phone app. The identifier is required to
+     * tell the WNS to which application a push notification should be sent. */
+    private String SID = null;
+
+    /** The client secret which is specific to a certain Windows Phone app and is required for the authentication at
+     * the WNS. */
+    private String wpClientSecret = null;
+
     /** A hash map which stores all cached push messages. */
     private ConcurrentHashMap<PushMessage, Integer> cachedPushMessages;
+
+    /** The access token which authenticates the server at the WNS. */
+    private static String wnsAccessToken = null;
+
+    private static ReentrantReadWriteLock wnsAccessTokenLock = new ReentrantReadWriteLock();
 
     /** A reference for the PushManager Singleton class. */
     private static PushManager _instance = new PushManager();
@@ -56,7 +72,8 @@ public class PushManager {
         Properties pushCredentials = retrievePushCredentials();
         if (pushCredentials != null) {
             GCM_API_KEY = pushCredentials.getProperty("gcmApiKey");
-            // TODO Read Windows stuff.
+            SID = pushCredentials.getProperty("sid");
+            wpClientSecret = pushCredentials.getProperty("wpSecret");
         } else {
             logger.error("PushManager was unable to read the push credentials of the properties object.");
         }
@@ -340,13 +357,124 @@ public class PushManager {
     }
 
     /**
-     * Sends the given JSON push message to the Android clients. TODO
+     * Sends the given JSON push message to the Windows clients which are identified by the given push tokens.
      *
      * @param pushTokens A list of Windows push tokens.
      * @param jsonPushMessage The push message as a JSON String.
      */
     private void notifyWindows(List<String> pushTokens, String jsonPushMessage) {
-        // TODO
+        // Check if there is at least one recipients. Do nothing if there is non.
+        if(pushTokens.isEmpty()){
+            logger.info("No Windows push tokens given. No Windows Phone user will be notified.");
+            return;
+        }
+        logger.info("Got a list of {} windows push tokens.", pushTokens.size());
+
+        // Read the access token.
+        String accessToken = null;
+        wnsAccessTokenLock.readLock().lock();
+        try{
+            accessToken = wnsAccessToken;
+        }finally {
+            wnsAccessTokenLock.readLock().unlock();
+        }
+
+        if(accessToken == null){
+            // Request a new access token and store it in the variable.
+            setWnsAccessToken();
+        }
+
+        // If sending fails, retry just once according to the best practices.
+        int maxRetries = 2;
+        int amountOfSuccessfulPushs = 0;
+        for (String pushToken : pushTokens) {
+            boolean successful = false;
+            int attempts = 0;
+            while(!successful && attempts < maxRetries){
+                wnsAccessTokenLock.readLock().lock();
+                try{
+                    accessToken = wnsAccessToken;
+                }finally {
+                    wnsAccessTokenLock.readLock().unlock();
+                }
+                // Try to send the push notification to the device identified by the given push token.
+                successful = sendWindowsRawNotification(pushToken, jsonPushMessage, accessToken);
+                attempts++;
+            }
+
+            if(successful){
+                amountOfSuccessfulPushs++;
+                logger.debug("Successfully sent the push notification to the client identified by the push token: {}" +
+                        ".", pushToken);
+            }else{
+                logger.debug("Sending to token:{} has failed.", pushToken);
+            }
+        }
+        logger.info("Sent push messages to {} windows clients.", amountOfSuccessfulPushs);
+    }
+
+    /**
+     * Sends a raw push notification to the device which is identified by the determined push token.
+     *
+     * @param pushToken The push token which identifies the client device.
+     * @param content The content of the raw notification.
+     * @param accessToken The access token which identifies the server at the WNS.
+     * @return Returns true if the notification has been sent successfully, false otherwise.
+     */
+    private boolean sendWindowsRawNotification(String pushToken, String content, String accessToken){
+        boolean successful = true;
+        try {
+            URL urlFromToken = new URL(pushToken);
+            HttpURLConnection conn = (HttpURLConnection) urlFromToken.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("X-WNS-Type", "wns/raw");
+            conn.setRequestProperty("X-WNS-Cache-Policy", "cache");
+            conn.setRequestProperty("Content-Type", "application/octet-stream");
+            conn.setRequestProperty("Authorization", String.format("Bearer %s", accessToken));
+            conn.setDoOutput(true);
+
+            // Write the HTTP request content.
+            OutputStream out = conn.getOutputStream();
+            out.write(content.getBytes());
+            out.flush();
+            out.close();
+
+            // Read the response.
+            int responseCode = conn.getResponseCode();
+            switch(responseCode){
+                case HttpURLConnection.HTTP_UNAUTHORIZED:
+                    logger.warn("The raw notification request to the WNS has failed. The access token is invalid.");
+                    // The access token is invalid. Request a new one.
+                    setWnsAccessToken();
+                    successful = false;
+                    break;
+                case HttpURLConnection.HTTP_GONE:
+                    logger.warn("The push token is invalid. Could not send a notification. Request won't be retried.");
+                    break;
+                case HttpURLConnection.HTTP_NOT_FOUND:
+                    logger.warn("The push token is invalid. Could not send a notification. Request won't be retried.");
+                    break;
+                case HttpURLConnection.HTTP_NOT_ACCEPTABLE:
+                    logger.warn("The WNS throttles this channel due to to many push notifications in a short amount " +
+                            "of time. Request won't be retried.");
+                    break;
+                default:
+                    logger.error("Could not send push notification: Response code is: {}, debug trace is: {}, error " +
+                            "description is {}, msg id: {}, wns status: {}.", responseCode, conn.getHeaderField
+                            ("X-WNS-Debug-Trace"), conn.getHeaderField("X-WNS-Error-Description"), conn
+                            .getHeaderField("X-WNS-Msg-ID"), conn.getHeaderField("X-WNS-Status"));
+                    successful = false;
+                    break;
+            }
+
+        } catch (MalformedURLException e) {
+            logger.error("The push token could not be parsed to a valid url.");
+        } catch (IOException e) {
+            logger.error("Error appeared during the sending process of a push notification to the WNS.");
+            successful = false;
+        }
+
+        return successful;
     }
 
     /**
@@ -359,5 +487,112 @@ public class PushManager {
         /*
         NOTE: The iOS methods won't be implemented in this project.
          */
+    }
+
+    /**
+     * A helper method which requests a new WNS access token and stores it to the static variable wnsAccessToken. The
+     * method is implemented thread-safe. Only one thread can request a new access token at a time. If multiple
+     * threads try to request an access token, only one of them will get the lock and the others will immediately
+     * leave the method.
+     */
+    private void setWnsAccessToken(){
+        // Request a new access token. Lock so no other thread can access the request token at the same time.
+        if(wnsAccessTokenLock.writeLock().tryLock()){
+            try{
+                // Request the new token and store it to the variable.
+                logger.info("Requested a new access token.");
+                wnsAccessToken = getWindowsNotificationServiceAccessToken(wpClientSecret, SID);
+            } finally {
+                wnsAccessTokenLock.writeLock().unlock();
+            }
+        }
+    }
+
+    /**
+     * Performs the authorization of the server with the WNS. Requests a valid access token from the WNS which can be
+     * used for push notification requests. Returns the access token if the authorization is successful, otherwise
+     * the method returns null.
+     *
+     * @param secret The client secret which provides the permission to send push notifications for this app.
+     * @param sid The package security identifier which identifies the app.
+     * @return The access token for the WNS or null if authorization fails.
+     */
+    private String getWindowsNotificationServiceAccessToken(String secret, String sid){
+        String accessToken = null;
+        try {
+            // Encode the secret and the sid to fit the x-www-form-urlencoded format.
+            secret = URLEncoder.encode(secret, "UTF-8");
+            sid = URLEncoder.encode(sid, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error("The Windows App secret or sid could not be encoded into a valid URL format.");
+            return null;
+        }
+
+        // Create the content of the Http request.
+        String content = String.format("grant_type=client_credentials&client_id=%s&client_secret=%s&scope=notify" +
+                ".windows.com", sid, secret);
+
+        try {
+            URL url = new URL("https://login.live.com/accesstoken.srf");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setDoOutput(true);
+
+            // Set the content of the request.
+            OutputStream outputStream = conn.getOutputStream();
+            outputStream.write(content.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+            // Read the response of the HTTP Request.
+            int responseStatus = conn.getResponseCode();
+            if(responseStatus == HttpURLConnection.HTTP_OK){
+                // Read the response content.
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while((inputLine = in.readLine()) != null){
+                    response.append(inputLine);
+                }
+                in.close();
+                logger.debug("Response from WNS:{}.", response.toString());
+
+                // Extract the accessToken form the json string.
+                accessToken = retrieveWNSAccessTokenFromJSON(response.toString());
+            }else{
+                logger.warn("The Authorization request to the WNS didn't provide the expected 200 OK status. No " +
+                        "access token will be returned.");
+            }
+
+        } catch (MalformedURLException e) {
+            logger.error("The WNS URL for the Server authentication is not valid.");
+        } catch (IOException e) {
+            logger.error("An IOException occurred during the authorization request. The authorization to WNS has " +
+                    "failed.");
+        }
+
+        return accessToken;
+    }
+
+    /**
+     * Parses the JSON string which has been received from the WNS. Searches for the property of access_token and
+     * returns the corresponding value as a string.
+     *
+     * @param jsonString The JSON string.
+     * @return The access token or null if the access token could not be retrieved.
+     */
+    private String retrieveWNSAccessTokenFromJSON(String jsonString){
+        JSONTokener tokener = new JSONTokener(jsonString);
+        JSONObject jsonObject = new JSONObject(tokener);
+        String accessToken;
+        try{
+            accessToken = jsonObject.getString("access_token");
+        }catch(JSONException e){
+            logger.warn("Unable to retrieve the access token from the WNS response JSON string. The method will " +
+                    "return null.");
+            accessToken = null;
+        }
+        return accessToken;
     }
 }
